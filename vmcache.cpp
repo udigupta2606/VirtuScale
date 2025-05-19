e817f7f5c82c970d5ccce0cb2411da37cfe3df29
ayushraturii <ayushraturi2222@gmail.com>
2025-05-19 22:35:53 +0530

    Update

--- vmcache.cpp
+++ vmcache.cpp
@@ -1,182 +1,330 @@
-
-// utilities 
-#include <algorithm> 
-#include <functional> 
+#include <atomic>
+#include <algorithm>
+#include <cassert>
+#include <csignal>
+#include <exception>
+#include <fcntl.h>
+#include <functional>
+#include <iostream>
+#include <mutex>
 #include <numeric>
 #include <set>
+#include <thread>
 #include <vector>
-#include <span> //for span, a lightweight array view
-#include <iostream> //for input and output
+#include <span>
 
-// time and error handling
-#include <exception> //for exception handling
-#include <sys/time.h> //for time-related functions
-#include <errno.h>  //for error handling
-#include <cassert> //to catch incorrect assumptions like invalid state
-#include <csignal> //for signal handling to intercept page faults
+#include <errno.h>
+#include <libaio.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <immintrin.h>
 
-// multithreading and synchronization 
-#include <atomic> //provide atomic operations
-#include <mutex> //for mutexes
-#include <thread> //for threads 
-
-//memory management
-#include <sys/mman.h> //for memory management declarations
-#include <immintrin.h> //for Intel intrinsics
-
-// file device io
-#include <fcntl.h> //for file control options
-#include <libaio.h> //for asynchronous I/O
-#include <sys/ioctl.h> //for ioctl system call for device-specific input/output operations
-#include <sys/stat.h> //for file status
-#include <sys/types.h> //for data types used in system calls
-#include <unistd.h> //for POSIX operating system API POSIX  "Portable Operating System Interface" 
-
-//expmap
-#include "expmap.h" //for expmap library
-
-#include "tpcc/TPCCWorkload.h" //for TPCC workload 
-
-using namespace std;
+#include "exmap.h"
 
 __thread uint16_t workerThreadId = 0;
 __thread int32_t tpcchistorycounter = 0;
+#include "tpcc/TPCCWorkload.hpp"
 
-// typedefs
+using namespace std;
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef u64 PID; //page id type
+typedef u64 PID; // page id type
 
-static const u64 pageSize=4096; //page size 4kb 
+static const u64 pageSize = 4096;
 
-struct alignas(4096) Page{
-    bool dirty; //indicates if the page is dirty
+struct alignas(4096) Page {
+   bool dirty;
 };
 
-static const int16_t maxWorkerThreads=128; //maximum number of worker threads
+static const int16_t maxWorkerThreads = 128;
 
-#define die(msg) do{ perror(msg); exit(EXIT_FAILURE);}while(0) //macro to handle error & exit program
+#define die(msg) do { perror(msg); exit(EXIT_FAILURE); } while(0)
 
-u64 rdtsc() { //function to read the time-stamp counter
-    u32 lo, hi;
-    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-    return static_cast<u64>(lo)|(static_cast<u64>(hi)<<32);
+
+typedef u64 KeyType;
+
+void handleSEGFAULT(int signo, siginfo_t* info, void* extra) {
+   void* page = info->si_addr;
+   if (bm.isValidPtr(page)) {
+      cerr << "segfault restart " << bm.toPID(page) << endl;
+      throw OLCRestartException();
+   } else {
+      cerr << "segfault " << page << endl;
+      _exit(1);
+   }
 }
 
-// exmap helper function 
-static int exmapAction(int exmapfd,exmap_opcode op ,u16 len)
+template <class Record>
+struct vmcacheAdapter
 {
-    struct exmap_action_params params_free={ 
-        .interface=workerThreadId,
-        .iov_len=len,
-        .opcode=(u16)op,
-    };
-    return ioctl(exmapfd,EXMAP_ACTION,&params_free); //perform the action on the exmap file descriptor
-}
+   BTree tree;
 
-//alocate memory using huge pages
-void* allocHuge(size_t size){
-    void* p=mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0); //map memory
-    madvise(p,size,MADV_HUGEPAGE); //advise the kernel to use huge pages
-    return p; //return the pointer to the allocated memory
-}
+   public:
+   void scan(const typename Record::Key& key, const std::function<bool(const typename Record::Key&, const Record&)>& found_record_cb, std::function<void()> reset_if_scan_failed_cb) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      u8 kk[Record::maxFoldLength()];
+      tree.scanAsc({k, l}, [&](BTreeNode& node, unsigned slot) {
+         memcpy(kk, node.getPrefix(), node.prefixLen);
+         memcpy(kk+node.prefixLen, node.getKey(slot), node.slot[slot].keyLen);
+         typename Record::Key typedKey;
+         Record::unfoldKey(kk, typedKey);
+         return found_record_cb(typedKey, *reinterpret_cast<const Record*>(node.getPayload(slot).data()));
+      });
+   }
+   // -------------------------------------------------------------------------------------
+   void scanDesc(const typename Record::Key& key, const std::function<bool(const typename Record::Key&, const Record&)>& found_record_cb, std::function<void()> reset_if_scan_failed_cb) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      u8 kk[Record::maxFoldLength()];
+      bool first = true;
+      tree.scanDesc({k, l}, [&](BTreeNode& node, unsigned slot, bool exactMatch) {
+         if (first) { // XXX: hack
+            first = false;
+            if (!exactMatch)
+               return true;
+         }
+         memcpy(kk, node.getPrefix(), node.prefixLen);
+         memcpy(kk+node.prefixLen, node.getKey(slot), node.slot[slot].keyLen);
+         typename Record::Key typedKey;
+         Record::unfoldKey(kk, typedKey);
+         return found_record_cb(typedKey, *reinterpret_cast<const Record*>(node.getPayload(slot).data()));
+      });
+   }
+   // -------------------------------------------------------------------------------------
+   void insert(const typename Record::Key& key, const Record& record) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      tree.insert({k, l}, {(u8*)(&record), sizeof(Record)});
+   }
+   // -------------------------------------------------------------------------------------
+   template<class Fn>
+   void lookup1(const typename Record::Key& key, Fn fn) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      bool succ = tree.lookup({k, l}, [&](span<u8> payload) {
+         fn(*reinterpret_cast<const Record*>(payload.data()));
+      });
+      assert(succ);
+   }
+   // -------------------------------------------------------------------------------------
+   template<class Fn>
+   void update1(const typename Record::Key& key, Fn fn) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      tree.updateInPlace({k, l}, [&](span<u8> payload) {
+         fn(*reinterpret_cast<Record*>(payload.data()));
+      });
+   }
+   // -------------------------------------------------------------------------------------
+   // Returns false if the record was not found
+   bool erase(const typename Record::Key& key) {
+      u8 k[Record::maxFoldLength()];
+      u16 l = Record::foldKey(k, key);
+      return tree.remove({k, l});
+   }
+   // -------------------------------------------------------------------------------------
+   template <class Field>
+   Field lookupField(const typename Record::Key& key, Field Record::*f) {
+      Field value;
+      lookup1(key, [&](const Record& r) { value = r.*f; });
+      return value;
+   }
 
-//use when lock is not free
-void yield(u64 counter){
-    _mm_pause(); //pause the thread for a short period
-}
+   u64 count() {
+      u64 cnt = 0;
+      tree.scanAsc({(u8*)nullptr, 0}, [&](BTreeNode& node, unsigned slot) { cnt++; return true; } );
+      return cnt;
+   }
 
-struct PageState{
-    atomic<u64> stateAndVersion; //atomic variable to store state and version
-
-    static const u64 Unlocked=0; //unlocked state
-    static const u64 MaxShared=252; //maximum shared state
-    static const u63 Locked=253; //locked state
-    static const u64 Marked=254; //marked state
-    static const u64 Evicted=255; //evicted state
-
-    PageState(){}
-
-    void init(){
-        stateAndVersion.store(sameVersion(0,Evicted),memory_order_release); //initialize the state and version
-    }
-
-    static inline u64 sameVersion(u64 oldStateAndVersion,u64 newState){
-        return ((oldStateAndVersion<<8)>>8)|newState<<56;}
-    }
-
-    static inline u64 nextVersion(u64 oldStateAndVersion,u64 newState){
-        return (((oldStateAndVersion<<8)>>8)+1)|newState<<56;
-    }
-
-    bool tryLockX(u64 oldStateAndVersion)
-    {
-        return stateAndVersion.compare_exchange_strong(oldStateAndVersion,sameVersion(oldStateAndVersion,Locked));
-    }
-
-    void unlockX()
-    {
-        assert(getState() == Locked);
-        stateAndVersion.store(nextVersion(stateAndVersion.load(),Unlocked),memory_order_release);
-    }
-
-    void unlockXEvicted(){
-        assert(getState() == Locked);
-        stateAndVersion.store(nextVersion(stateAndVersion.load(),Evicted),memory_order_release);
-    }
-
-    void downgradeLock(){
-        assert(getState() == Locked);
-        stateAndVersion.store(nextVersion(stateAndVersion.load(),1),memory_order_release);
-    }
-
-    bool tryLockS(u64 oldStateAndVersion)
-    {
-        u64 s=getState(oldStateAndVersion);
-        if(s<MaxShared)
-        {
-            return stateAndVersion.compare_exchange_strong(oldStateAndVersion,sameVersion(oldStateAndVersion,s+1));
-        }
-        if(s==Marked)
-        {
-            return stateAndVersion.compare_exchange_strong(oldStateAndVersion,sameVersion(oldStateAndVersion,1));
-        }
-        return false;
-    }
-
-    void unlockS(){
-        while(true)
-        {
-            u64 oldStateAndVersion=stateAndVersion.load();
-            u64 state=getState(oldStateAndVersion);
-            assert(state>0 && state<=MaxShared);
-            if (stateAndVersion.compare_exchange_strong(oldStateAndVersion,sameVersion(oldStateAndVersion,state-1)))
-            {
-                return;
-            }   
-        }
-    }
-    bool tryMark(u64 oldStateAndVersion) {
-        assert(getState(oldStateAndVersion)==Unlocked);
-        return stateAndVersion.compare_exchange_strong(oldStateAndVersion, sameVersion(oldStateAndVersion, Marked));
-     }
-  
-     static u64 getState(u64 v) { return v >> 56; };
-     u64 getState() { return getState(stateAndVersion.load()); }
-  
-     void operator=(PageState&) = delete;
-
+   u64 countw(Integer w_id) {
+      u8 k[sizeof(Integer)];
+      fold(k, w_id);
+      u64 cnt = 0;
+      u8 kk[Record::maxFoldLength()];
+      tree.scanAsc({k, sizeof(Integer)}, [&](BTreeNode& node, unsigned slot) {
+         memcpy(kk, node.getPrefix(), node.prefixLen);
+         memcpy(kk+node.prefixLen, node.getKey(slot), node.slot[slot].keyLen);
+         if (memcmp(k, kk, sizeof(Integer))!=0)
+            return false;
+         cnt++;
+         return true;
+      });
+      return cnt;
+   }
 };
 
+template<class Fn>
+void parallel_for(uint64_t begin, uint64_t end, uint64_t nthreads, Fn fn) {
+   std::vector<std::thread> threads;
+   uint64_t n = end-begin;
+   if (n<nthreads)
+      nthreads = n;
+   uint64_t perThread = n/nthreads;
+   for (unsigned i=0; i<nthreads; i++) {
+      threads.emplace_back([&,i]() {
+         uint64_t b = (perThread*i) + begin;
+         uint64_t e = (i==(nthreads-1)) ? end : (b+perThread);
+         fn(i, b, e);
+      });
+   }
+   for (auto& t : threads)
+      t.join();
+}
 
+int main(int argc, char** argv) {
+   if (bm.useExmap) {
+      struct sigaction action;
+      action.sa_flags = SA_SIGINFO;
+      action.sa_sigaction = handleSEGFAULT;
+      if (sigaction(SIGSEGV, &action, NULL) == -1) {
+         perror("sigusr: sigaction");
+         exit(1);
+      }
+   }
 
+   unsigned nthreads = envOr("THREADS", 1);
+   u64 n = envOr("DATASIZE", 10);
+   u64 runForSec = envOr("RUNFOR", 30);
+   bool isRndread = envOr("RNDREAD", 0);
 
+   u64 statDiff = 1e8;
+   atomic<u64> txProgress(0);
+   atomic<bool> keepRunning(true);
+   auto systemName = bm.useExmap ? "exmap" : "vmcache";
 
+   auto statFn = [&]() {
+      cout << "ts,tx,rmb,wmb,system,threads,datasize,workload,batch" << endl;
+      u64 cnt = 0;
+      for (uint64_t i=0; i<runForSec; i++) {
+         sleep(1);
+         float rmb = (bm.readCount.exchange(0)*pageSize)/(1024.0*1024);
+         float wmb = (bm.writeCount.exchange(0)*pageSize)/(1024.0*1024);
+         u64 prog = txProgress.exchange(0);
+         cout << cnt++ << "," << prog << "," << rmb << "," << wmb << "," << systemName << "," << nthreads << "," << n << "," << (isRndread?"rndread":"tpcc") << "," << bm.batch << endl;
+      }
+      keepRunning = false;
+   };
 
+   if (isRndread) {
+      BTree bt;
+      bt.splitOrdered = true;
 
+      {
+         // insert
+         parallel_for(0, n, nthreads, [&](uint64_t worker, uint64_t begin, uint64_t end) {
+            workerThreadId = worker;
+            array<u8, 120> payload;
+            for (u64 i=begin; i<end; i++) {
+               union { u64 v1; u8 k1[sizeof(u64)]; };
+               v1 = __builtin_bswap64(i);
+               memcpy(payload.data(), k1, sizeof(u64));
+               bt.insert({k1, sizeof(KeyType)}, payload);
+            }
+         });
+      }
+      cerr << "space: " << (bm.allocCount.load()*pageSize)/(float)bm.gb << " GB " << endl;
 
+      bm.readCount = 0;
+      bm.writeCount = 0;
+      thread statThread(statFn);
 
+      parallel_for(0, nthreads, nthreads, [&](uint64_t worker, uint64_t begin, uint64_t end) {
+         workerThreadId = worker;
+         u64 cnt = 0;
+         u64 start = rdtsc();
+         while (keepRunning.load()) {
+            union { u64 v1; u8 k1[sizeof(u64)]; };
+            v1 = __builtin_bswap64(RandomGenerator::getRand<u64>(0, n));
 
+            array<u8, 120> payload;
+            bool succ = bt.lookup({k1, sizeof(u64)}, [&](span<u8> p) {
+               memcpy(payload.data(), p.data(), p.size());
+            });
+            assert(succ);
+            assert(memcmp(k1, payload.data(), sizeof(u64))==0);
 
+            cnt++;
+            u64 stop = rdtsc();
+            if ((stop-start) > statDiff) {
+               txProgress += cnt;
+               start = stop;
+               cnt = 0;
+            }
+         }
+         txProgress += cnt;
+      });
+
+      statThread.join();
+      return 0;
+   }
+   
+   // TPC-C
+   Integer warehouseCount = n;
+
+   vmcacheAdapter<warehouse_t> warehouse;
+   vmcacheAdapter<district_t> district;
+   vmcacheAdapter<customer_t> customer;
+   vmcacheAdapter<customer_wdl_t> customerwdl;
+   vmcacheAdapter<history_t> history;
+   vmcacheAdapter<neworder_t> neworder;
+   vmcacheAdapter<order_t> order;
+   vmcacheAdapter<order_wdc_t> order_wdc;
+   vmcacheAdapter<orderline_t> orderline;
+   vmcacheAdapter<item_t> item;
+   vmcacheAdapter<stock_t> stock;
+
+   TPCCWorkload<vmcacheAdapter> tpcc(warehouse, district, customer, customerwdl, history, neworder, order, order_wdc, orderline, item, stock, true, warehouseCount, true);
+
+   {
+      tpcc.loadItem();
+      tpcc.loadWarehouse();
+
+      parallel_for(1, warehouseCount+1, nthreads, [&](uint64_t worker, uint64_t begin, uint64_t end) {
+         workerThreadId = worker;
+         for (Integer w_id=begin; w_id<end; w_id++) {
+            tpcc.loadStock(w_id);
+            tpcc.loadDistrinct(w_id);
+            for (Integer d_id = 1; d_id <= 10; d_id++) {
+               tpcc.loadCustomer(w_id, d_id);
+               tpcc.loadOrders(w_id, d_id);
+            }
+         }
+      });
+   }
+   cerr << "space: " << (bm.allocCount.load()*pageSize)/(float)bm.gb << " GB " << endl;
+
+   bm.readCount = 0;
+   bm.writeCount = 0;
+   thread statThread(statFn);
+
+   parallel_for(0, nthreads, nthreads, [&](uint64_t worker, uint64_t begin, uint64_t end) {
+      workerThreadId = worker;
+      u64 cnt = 0;
+      u64 start = rdtsc();
+      while (keepRunning.load()) {
+         int w_id = tpcc.urand(1, warehouseCount); // wh crossing
+         tpcc.tx(w_id);
+         cnt++;
+         u64 stop = rdtsc();
+         if ((stop-start) > statDiff) {
+            txProgress += cnt;
+            start = stop;
+            cnt = 0;
+         }
+      }
+      txProgress += cnt;
+   });
+
+   statThread.join();
+   cerr << "space: " << (bm.allocCount.load()*pageSize)/(float)bm.gb << " GB " << endl;
+
+   return 0;
+}
